9caad9da7f7bed6abc9386b9b49524d9c14cd1cb
Igor Korsukov <igor.korsukov@gmail.com>
2023-12-11 23:58:19 +0200

    added flutter CI

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'shoplist/view/edit_screen.dart';
 // flutter build apk --split-per-abi
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  //await dotenv.load(fileName: ".env");
 
   final List<ModuleSetup> modules = [InfrastructureModule(), ShopListModule()];
   for (var m in modules) {
